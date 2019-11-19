ebb76fa18efa4fee44f6673d2f346bb87f77251a
LucyLavend <jongebloedlucy@gmail.com>
2019-11-19 14:58:37 +0100

    initial commit

---
+++ vixel/myscene.cpp
@@ -0,0 +1,76 @@
+/**
+ * This class describes MyScene behavior.
+ *
+ * Copyright 2015 Your Name <you@yourhost.com>
+ */
+
+#include <fstream>
+#include <sstream>
+
+#include "myscene.h"
+int xSpeed = 300;
+int ySpeed = 300;
+
+MyScene::MyScene() : Scene()
+{
+	// start the timer.
+	t.start();
+
+	// create a single instance of MyEntity in the midadle of the screen.
+	// the Sprite is added in Constructor of MyEntity.
+	myentity = new MyEntity();
+	myentity->position = Point2(SWIDTH/2, SHEIGHT/2);
+
+	// create the scene 'tree'
+	// add myentity to this Scene as a child.
+	this->addChild(myentity);
+}
+
+
+MyScene::~MyScene()
+{
+	// deconstruct and delete the Tree
+	this->removeChild(myentity);
+
+	// delete myentity from the heap (there was a 'new' in the constructor)
+	delete myentity;
+}
+
+void MyScene::update(float deltaTime)
+{
+	// ###############################################################
+	// Escape key stops the Scene
+	// ###############################################################
+	if (input()->getKeyUp(KeyCode::Escape)) {
+		this->stop();
+	}
+
+	// ###############################################################
+	// Spacebar scales myentity
+	// ###############################################################
+	if (input()->getKeyDown(KeyCode::Space)) {
+		myentity->scale = Point(0.5f, 0.5f);
+	}
+	if (input()->getKeyUp(KeyCode::Space)) {
+		myentity->scale = Point(1.0f, 1.0f);
+	}
+
+	myentity->position.x += xSpeed * deltaTime;
+	myentity->position.y += ySpeed * deltaTime;
+
+	if (myentity->position.x >= SWIDTH || myentity->position.x <= 0) {
+		xSpeed *= -1;
+	}
+	if (myentity->position.y >= SHEIGHT || myentity->position.y <= 0) {
+		ySpeed *= -1;
+	}
+	//ste
+	// ###############################################################
+	// Rotate color
+	// ###############################################################
+	if (t.seconds() > 0.0333f) {
+		RGBAColor color = myentity->sprite()->color;
+		myentity->sprite()->color = Color::rotate(color, 0.01f);
+		t.start();
+	}
+}
