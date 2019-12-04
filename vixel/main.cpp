7497be47d4bd991dde6132ae830d514b630f7110
LucyLavend <jongebloedlucy@gmail.com>
2019-12-04 09:19:56 +0100

    add Space Invaders demo for reference

--- vixel/main.cpp
+++ vixel/main.cpp
@@ -1,36 +1,44 @@
 /**
- * Copyright 2015 Your Name <you@yourhost.com>
+ * This file is part of a demo that shows how to use RT2D, a 2D OpenGL framework.
  *
- * @brief Description of My Awesome Game.
- *
- * @file main.cpp
- *
- * @mainpage My Awesome Game
- *
- * @section intro Introduction
- *
- * Detailed description of My Awesome Game.
- *
- * There's even a second paragraph.
+ * - Copyright 2015 Rik Teerling <rik@onandoffables.com>
+ *     - Initial commit
  */
+
 #include <rt2d/core.h>
 
-#include "myscene.h"
+#include "level01.h"
 
-/// @brief main entry point
 int main( void )
 {
 	// Core instance
 	Core core;
 
-	// Scene01
-	MyScene* myscene = new MyScene(); // create Scene on the heap
-	while(myscene->isRunning()) { // check status of Scene every frame
-		core.run(myscene); // update and render the current scene
+	// Create all scenes on the heap and keep a list
+	std::vector<SuperScene*> scenes;
+	scenes.push_back(new Level01()); // canvas space invaders
+	int s = scenes.size();
+
+	// start running with the first Scene
+	SuperScene* scene = scenes[0];
+	int scenecounter = 0;
+	int running = 1;
+	while (running) {
+		scenecounter = scene->activescene;
+		if (scenecounter > s-1) { scenecounter = 0; scene->activescene = 0; }
+		if (scenecounter < 0) { scenecounter = s-1; scene->activescene = s-1; }
+		scene = scenes[scenecounter];
+		core.run(scene); // update and render the current scene
 		core.showFrameRate(5); // show framerate in output every n seconds
+		if (!scene->isRunning()) { running = 0; } // check status of Scene every frame
 	}
-	//core.cleanup(); // cleanup ResourceManager (Textures + Meshes, but not Shaders)
-	delete myscene; // delete Scene and everything in it from the heap to make space for next Scene
+
+	// delete all scenes
+	for (int i = 0; i < s; i++) {
+		delete scenes[i];
+		scenes[i] = nullptr;
+	}
+	scenes.clear();
 
 	// No need to explicitly clean up the core.
 	// As a local var, core will go out of scope and destroy Renderer->ResourceManager.
