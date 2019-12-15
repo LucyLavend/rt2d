fd25aaf09d0a9ca1acca25215d6659f09eded4ba
LucyLavend <jongebloedlucy@gmail.com>
2019-12-15 20:28:33 +0100

    Add character files

---
+++ vixel/character.cpp
@@ -0,0 +1,19 @@
+/**
+ * This file is part of a demo that shows how to use RT2D, a 2D OpenGL framework.
+ *
+ * - Copyright 2015 Rik Teerling <rik@onandoffables.com>
+ *     - Initial commit
+ */
+
+#include "character.h"
+
+SuperScene::SuperScene()
+{
+	
+}
+
+
+SuperScene::~SuperScene()
+{
+
+}
---
+++ vixel/character.h
@@ -0,0 +1,22 @@
+/**
+ * This file is part of a demo that shows how to use RT2D, a 2D OpenGL framework.
+ *
+ * - Copyright 2015 Rik Teerling <rik@onandoffables.com>
+ *     - Initial commit
+ */
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <vector>
+
+class Character: public Character
+{
+public:
+
+
+private:
+
+};
+
+#endif /* CHARACTER_H */
